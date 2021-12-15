f41fda6c2786f039c4238d6e174cd1de2f6a8295
Benjamín von Potobsky <bvonpotobsky@gmail.com>
2021-12-14 22:02:52 -0300

    Backoffice added
    You can see the list of assistants from here. Login still needs to be made

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from "react";
-
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "@pages/Home";
+import Login from "@pages/Login";
+import Backoffice from "@pages/Backoffice";
 
 const App = () => {
   return (
     <React.StrictMode>
-      <Home />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/auth" element={<Login />} />
+          <Route path="/auth/backoffice" element={<Backoffice />} />
+        </Routes>
+      </Router>
     </React.StrictMode>
   );
 };
---
+++ src/pages/Backoffice.js
@@ -0,0 +1,64 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+import "@styles/pages/Backoffice.scss";
+
+const Backoffice = () => {
+  const [assistants, setAssistants] = useState([]);
+
+  useEffect(async () => {
+    try {
+      const response = await axios.get(
+        "http://localhost:3005/api/v1/assistant"
+      );
+
+      const { data } = response;
+      setAssistants(data);
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+
+  const qtyAssistants = assistants.length;
+
+  return (
+    <section className="Backoffice">
+      <div className="Backoffice__container">
+        <h1 className="Backoffice__title">Lista de asistentes</h1>
+
+        <button className="Backoffice__button">Descargar lista</button>
+
+        {assistants?.length > 0 ? (
+          <div className="Backoffice__table">
+            <p className="Backoffice__counter">
+              Hay <b>{qtyAssistants}</b> inscriptos por el momento
+            </p>
+
+            <div className="Backoffice__table__header">
+              <span>Nombre</span>
+              <span>Apellido</span>
+            </div>
+
+            <div className="Backoffice__table__body">
+              {assistants?.map((assistant) => (
+                <div
+                  key={assistant.id}
+                  className="Backoffice__table__body--item"
+                >
+                  <span>{assistant.firstName}</span>
+                  <span>{assistant.lastName}</span>
+                </div>
+              ))}
+            </div>
+          </div>
+        ) : (
+          <div className="Backoffice__empty">
+            <p>No hay asistentes registrados</p>
+          </div>
+        )}
+      </div>
+    </section>
+  );
+};
+
+export default Backoffice;
